b93411c08325a11fd9e5edf0cd1451e79170f115
1924951 <1924951@00-46273.pusd.dom>
2017-05-10 14:43:42 -0700

    update gui

--- FinalProjectStuff1/src/guiC.java
+++ FinalProjectStuff1/src/guiC.java
@@ -1,11 +1,20 @@
 import java.awt.*;          // access to Container
 import java.awt.event.*;    // access to WindowAdapter, WindowEvent
+
 import javax.swing.*;       // access to JFrame and JComponents
 import javax.swing.event.*;     // access to JSlider events
-import chn.util.*;
-import apcslib.Format;
+
 import java.util.*;
 
-public class guiC {
-
+public class guiC implements ActionListener{
+    private Unit myUnit;
+    public guiC()
+    {
+    	myUnit = new Unit();
+    }
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 }
