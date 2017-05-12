b8aa9be01e031cf8d0d7fccb51539a624cda9faf
1924951 <1924951@00-46273.pusd.dom>
2017-05-12 14:03:02 -0700

    Array for Player Units (incomplete)

---
+++ FinalProjectStuff1/src/TrackAry.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class TrackAry {
+private ArrayList<Unit> info;////(?)
+public TractAry()
+{
+	info = newArrayList<>;
+}
+public void makeUnit()
+{
+	info.add();///////ALL THE INFORMATION OF THE NEW UNIT
+}
+public ArrayList<Unit> getArray()
+{
+	return info;
+}
+}
