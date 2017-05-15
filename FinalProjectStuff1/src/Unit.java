546d7312bebcb3c36020755e80a3685e728e5d23
person1357 <alberthuali167@gmail.com>
2017-05-15 13:53:50 -0700

    Updated Unit class

--- FinalProjectStuff1/src/Unit.java
+++ FinalProjectStuff1/src/Unit.java
@@ -1,32 +1,15 @@
-public class Unit 
-{
+public class Unit {
 	private String name;
-	private Boolean isEnemy;
-	private Boolean canAttack;
-	private Boolean alive;
 	private int health;
 	private int damage;
-	private int speed;
 	private int cost;
-	private int attackDistance;
-	private String weaponType;
-
 	private int loc;
 
-	// String weaponType;
-
-	public Unit()
-	{
+	public Unit() {
 		name = "";
-		isEnemy = false;
-		alive =  true;
-		canAttack = true;
 		health = 0;
 		damage = 0;
-		speed = 0;
 		cost = 0;
-		attackDistance = 0;	
-		danger = false;
 	}
 
 	public void setName(String newName) {
@@ -36,15 +19,6 @@ public class Unit
 	public String getName() {
 		return name;
 	}
-
-	public void setSpeed(int nSpeed) {
-		speed = nSpeed;
-	}
-
-	public int getSpeed() {
-		return speed;
-	}
-
 	public void setHealth(int nHealth) {
 		health = nHealth;
 	}
@@ -52,16 +26,28 @@ public class Unit
 	public int getHealth() {
 		return health;
 	}
-/*	public void setDanger(boolean dLevel)
+	public void setDamage(int dmg)
 	{
-		danger = dLevel;
+		damage = dmg;
 	}
-	public boolean getDanger()
+	public int getDamage()
 	{
-		return danger;
+		return damage;
 	}
-	public int updateLoc()
-	{*/
-		
-
+	public void setCost(int c)
+	{
+		cost = c;
+	}
+	public int getCost()
+	{
+		return cost;
+	}
+	public void setLoc(int location)
+	{
+		loc = location;
+	}
+	public int getLoc()
+	{
+		return loc;
+	}
 }
--- FinalProjectStuff1/src/guiC.java
+++ FinalProjectStuff1/src/guiC.java
@@ -2,8 +2,6 @@ import java.awt.*;          // access to Container
 import java.awt.event.*;    // access to WindowAdapter, WindowEvent
 import javax.swing.*;       // access to JFrame and JComponents
 import javax.swing.event.*;     // access to JSlider events
-import chn.util.*;
-import apcslib.Format;
 import java.util.*;
 
 public class guiC {
