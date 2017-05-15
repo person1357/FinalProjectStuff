995e32d16f0a7afd2447c25a7e37451f8f926937
1924951 <1924951@00-46273.pusd.dom>
2017-05-15 13:59:48 -0700

    updating unitKiller, replacing trackAry with unitArray

--- FinalProjectStuff1/src/unitKiller.java
+++ FinalProjectStuff1/src/unitKiller.java
@@ -1,7 +1,12 @@
-
+import java.util.ArrayList;
+import java.util.*;
 public class unitKiller {
-public unitKiller(ArrayList<Unit> ary)
+public ArrayList<Unit> (ArrayList<Unit> ary)
 {
-	
+	for(int a = ary.size(); a > 0; a--)
+	{
+		if (ary.get(a).getHealth()==0)
+		{ary.remove(a);}
+	}
 }
 }
