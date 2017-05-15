a53e7fa234d6a5fddc0a6c098fde0e77900af0e7
1924951 <1924951@00-46273.pusd.dom>
2017-05-15 14:36:52 -0700

    updating runner main, gui

--- FinalProjectStuff1/src/RunnerClass.java
+++ FinalProjectStuff1/src/RunnerClass.java
@@ -1,7 +1,13 @@
+import javax.swing.JFrame;
+
 
 public class RunnerClass {
-	public static void main(String args[])
+
+	public static void main(String[] args)
 	{
-		
+	    guiC game = new guiC();
+	    JFrame f = new JFrame("MLG StickFight");
+	    game.initFrame(f);
 	}
+	
 }
--- FinalProjectStuff1/src/guiC.java
+++ FinalProjectStuff1/src/guiC.java
@@ -1,15 +1,24 @@
 import java.awt.*;          // access to Container
 import java.awt.event.*;    // access to WindowAdapter, WindowEvent
+
 import javax.swing.*;       // access to JFrame and JComponents
+import javax.swing.Timer;
 import javax.swing.event.*;     // access to JSlider events
+
 import java.util.*;
 
 public class guiC extends JFrame implements ActionListener{
     private Unit myUnit;
     private JButton startButton;
+    private RunnerClass run; //
     public guiC()
     {
-    	myUnit = new Unit();
+    	run = new RunnerClass();
+    }
+    
+    public void initFrame(JFrame frame)
+    {
+    	frame.setLayout(new GridLayout());
     	Timer timer = new javax.swing.Timer(1000, this);
     	startButton = new JButton("Start Game");
     }
