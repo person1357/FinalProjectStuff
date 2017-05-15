d3335a63f85c7d01eb855a9811a7479e62942b6d
1924951 <1924951@00-46273.pusd.dom>
2017-05-15 14:16:56 -0700

    updating GUI

--- FinalProjectStuff1/src/guiC.java
+++ FinalProjectStuff1/src/guiC.java
@@ -1,16 +1,17 @@
 import java.awt.*;          // access to Container
 import java.awt.event.*;    // access to WindowAdapter, WindowEvent
-
 import javax.swing.*;       // access to JFrame and JComponents
 import javax.swing.event.*;     // access to JSlider events
-
 import java.util.*;
 
-public class guiC implements ActionListener{
+public class guiC extends JFrame implements ActionListener{
     private Unit myUnit;
+    private JButton startButton;
     public guiC()
     {
     	myUnit = new Unit();
+    	Timer timer = new javax.swing.Timer(1000, this);
+    	startButton = new JButton("Start Game");
     }
 	@Override
 	public void actionPerformed(ActionEvent event) {
