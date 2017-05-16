89896bae1b29b6e96d7d424a562880504f6a67b8
1924951 <1924951@00-46273.pusd.dom>
2017-05-16 15:10:02 -0700

    update GUI

--- FinalProjectStuff1/src/guiC.java
+++ FinalProjectStuff1/src/guiC.java
@@ -10,25 +10,51 @@ import java.util.*;
 public class guiC extends JFrame implements ActionListener{
     private JButton startButton;
     private JButton unit1;
+    private JButton unit2;
+    private JButton unit3;
+    private JButton unit4;
+    private JButton unit5;
+    private JButton unit6;
+    private JButton unit7;
+    private JButton[] buttonArray;
     public guiC()
     {
+    	Container container = getContentPane();
+        container.setLayout( new GridLayout(5,9) );
     	
-    	startButton = new JButton("Start Game");
     	unit1 = new JButton("fish.gif");
+    	buttonArray = new JButton[8];
+    	ButtonHandler buttonHandler = new ButtonHandler();
     	// STEP 4: add unit JButtons
-    	add
+    	for(int a=1; a<8;a++)
+    	{
+    		buttonArray[a]= new JButton();
+    		container.add(buttonArray[a]);
+    	}
+    	startButton = new JButton("Start Game");
+    	container.add(startButton);
+    	startButton.addActionListener(buttonHandler);
     	// STEP 5: set default close operation
-        //setDefaultCloseOperation(System.EXIT_ON_CLOSE);
+    	addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                System.exit(0);
+            }
+        });
     	// STEP 6: set window size and show window
-        setSize(1000,700);
+        setSize(1000,1800);
         setVisible(true);
-        // construction a Swing timer that goes off every 1000 msec (1 sec)
-        Timer timer = new javax.swing.Timer(1000, this);
+        
     }
-    
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		
+		for(int a=1; a<8;a++)
+    	{
+    		if(event.getSource() == buttonArray[a])
+    			unitArray1.makeUnit(1);
+    	
+		if(event.getSource() == startButton)
+			// construction a Swing timer that goes off every 1000 msec (1 sec)
+	        Timer timer = new javax.swing.Timer(1000, this);
+			timer.start();		// timer starts here
 	}
-}
