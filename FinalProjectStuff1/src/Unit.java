931ab76afb00ed8a3a7559bb41345abbd7656d3b
1924951 <1924951@00-46273.pusd.dom>
2017-05-17 14:12:18 -0700

    update unit (by albert)

--- FinalProjectStuff1/src/Unit.java
+++ FinalProjectStuff1/src/Unit.java
@@ -1,53 +1,54 @@
 public class Unit {
-	private int id;
-	private int health;
-	private int damage;
-	private int cost;
-	private int loc;
+    private int id;
+    private int health;
+    private int damage;
+    private int cost;
+    private int loc;
 
-	public Unit() {
-		id = -1;
-		health = 0;
-		damage = 0;
-		cost = 0;
-	}
+    public Unit(int i,int h,int d,int c) {
+        id = i;
+        health = h;
+        damage = d;
+        cost = c;
+    }
+    public void setID(int n) {
+        id = n;
+    }
 
-	public void setID(int n) {
-		id = n;
-	}
+    public int getID() {
+        return id;
+    }
+    public void setHealth(int nHealth) {
+        health = nHealth;
+    }
 
-	public int getID() {
-		return id;
-	}
-	public void setHealth(int nHealth) {
-		health = nHealth;
-	}
-
-	public int getHealth() {
-		return health;
-	}
-	public void setDamage(int dmg)
-	{
-		damage = dmg;
-	}
-	public int getDamage()
-	{
-		return damage;
-	}
-	public void setCost(int c)
-	{
-		cost = c;
-	}
-	public int getCost()
-	{
-		return cost;
-	}
-	public void setLoc(int location)
-	{
-		loc = location;
-	}
-	public int getLoc()
-	{
-		return loc;
-	}
+    public int getHealth() {
+        return health;
+    }
+    public void setDamage(int dmg)
+    {
+        damage = dmg;
+    }
+    public int getDamage()
+    {
+        return damage;
+    }
+    public void setCost(int c)
+    {
+        cost = c;
+    }
+    public int getCost()
+    {
+        return cost;
+    }
+    public void setLoc(int location)
+    {
+        loc = location;
+    }
+    public int getLoc()
+    {
+        return loc;
+    }
 }
+
+
