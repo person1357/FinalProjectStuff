2daa6555756504926acb258ca1c53bb9237d2d12
1924951 <1924951@00-46273.pusd.dom>
2017-05-24 14:55:30 -0700

    gui2

--- FinalProjectStuff1/src/gui.java
+++ FinalProjectStuff1/src/gui.java
@@ -1,13 +1,198 @@
 import javax.swing.JPanel;
 
+import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+
+import javax.swing.JButton;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.BoxLayout;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Insets;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
+
+import java.awt.Component;
+
+import javax.swing.Box;
+
+import java.awt.SystemColor;
+import java.util.ArrayList;
+
 
 public class gui extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	private Timer timer;
+	private ArrayList<Integer> pAry;
+    private ArrayList<Integer> eAry;
+    private Image u11;
+    private Image u22;
+    private Image u33;
+    private Image u44;
+    private Image u55;
+    private Image u66;
+    private Image u77;
+    private Image u88;
+    private Image background;
+    private Updater update;
+    private Container container;
 	public gui() {
+		////////////////////////////
+		update = new Updater();
+        ClassLoader clder= this.getClass().getClassLoader();
+		ImageIcon u1 = new ImageIcon(clder.getResource("u1.jpg"));
+        ImageIcon u2 = new ImageIcon(clder.getResource("u2.jpg"));
+        ImageIcon u3 = new ImageIcon(clder.getResource("u3.jpg"));
+        u11=u1.getImage();
+        u22=u2.getImage();
+        u33=u3.getImage();
+        ////////////////////////////
+		setBackground(SystemColor.activeCaptionText);
+		
+		JButton btnStartGame = new JButton("Start Game");
+		
+		JButton button = new JButton(u1);
+		
+		JButton button_1 = new JButton("Unit");
+		
+		JButton button_2 = new JButton("Unit");
+		
+		JButton button_3 = new JButton("Unit");
+		
+		JButton button_4 = new JButton("Unit");
+		
+		JButton button_5 = new JButton("Unit");
+		
+		JButton button_6 = new JButton("Unit");
+		button_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JLabel lblUnits = new JLabel("Units");
+		
+		JTextArea txtrHealth = new JTextArea();
+		txtrHealth.setText("Health:");
+		
+		JTextArea txtrResource = new JTextArea();
+		txtrResource.setText("Resource:");
+		
+		JLabel label_1 = new JLabel("10");
+		
+		JLabel label_2 = new JLabel("30");
+		
+		JLabel label_3 = new JLabel("50");
+		
+		JLabel label_4 = new JLabel("75");
+		
+		JLabel label_5 = new JLabel("100");
+		
+		JLabel label_6 = new JLabel("150");
+		
+		JLabel label_7 = new JLabel("200");
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblUnits, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+									.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE))
+								.addComponent(txtrHealth, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(button, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+									.addGap(10)
+									.addComponent(btnStartGame, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE))
+								.addComponent(txtrResource, GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(113)
+							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(84)
+							.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(82)
+							.addComponent(label_3, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(84)
+							.addComponent(label_4, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(82)
+							.addComponent(label_5, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(81)
+							.addComponent(label_6, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(87)
+							.addComponent(label_7, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(txtrHealth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtrResource, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGap(408)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(label_1)
+								.addComponent(label_5)
+								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+									.addComponent(label_6)
+									.addComponent(label_7))))
+						.addComponent(label_2)
+						.addComponent(label_3)
+						.addComponent(label_4))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(button, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+							.addComponent(lblUnits, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(btnStartGame, GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+							.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)))
+					.addGap(20))
+		);
+		setSize(900,550);
+		setLayout(groupLayout);
 
 	}
-
 }
