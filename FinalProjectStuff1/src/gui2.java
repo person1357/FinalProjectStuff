7dc7f0d200e7cc1a1a24ba8c77da177f7b3b24ba
Yuanning Wu <cindywu0130@gmail.com>
2017-05-30 15:25:48 -0700

    this is working

--- FinalProjectStuff1/src/gui2.java
+++ FinalProjectStuff1/src/gui2.java
@@ -1,28 +1,41 @@
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+
+import java.awt.Component;
+
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
+import java.awt.event.*;    // access to WindowAdapter, WindowEvent
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.Timer;
-import javax.swing.border.EmptyBorder;
-// access to WindowAdapter, WindowEvent
-// access to WindowAdapter, WindowEvent
+import javax.swing.*; 
+
+import java.awt.event.*;    // access to WindowAdapter, WindowEvent
+
+import javax.swing.*; 
 
 public class gui2 extends JFrame {
-	
+
 	private JPanel contentPane;
 	private JButton SB;
 	private JButton u1;
@@ -36,26 +49,27 @@ public class gui2 extends JFrame {
 	private JLabel label;
 	private Updater update;
 	private ArrayList<Integer> pAry;
-	private ArrayList<Integer> eAry;
+    private ArrayList<Integer> eAry;
 	private Image u11;
-	private Image u22;
-	private Image u33;
-	private Image u44;
-	private Image u55;
-	private Image u66;
-	private Image u77;
-	private Image u88;
-	private Image background;
-	private Image baseP;
-	private Image baseE;
-	private GroupLayout gl_contentPane;
-	private JTextPane txtpnHealth;
-	private JTextPane txtpnResources;
-	private int r;
-	private boolean b1;
+    private Image u22;
+    private Image u33;
+    private Image u44;
+    private Image u55;
+    private Image u66;
+    private Image u77;
+    private Image u88;
+    //private Image background;
+    private Image baseP;
+    private Image baseE;
+    private GroupLayout gl_contentPane;
+    private JTextPane txtpnHealth;
+    private JTextPane txtpnResources;
+    private int r;
+    private boolean b1;
+    private boolean b2;
 	/**
-	* Launch the application.
-	*/
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -69,43 +83,44 @@ public class gui2 extends JFrame {
 		});
 	}
 	
-	
+
 	/**
-	* Create the frame.
-	*/
+	 * Create the frame.
+	 */
 	public gui2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 550);
+		System.out.println("hello");
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		//
 		update = new Updater();
-		ClassLoader clder= this.getClass().getClassLoader();
-		ImageIcon u1I = new ImageIcon(clder.getResource("unit1.jpg"));
-		ImageIcon u2I = new ImageIcon(clder.getResource("u2.jpg"));
-		ImageIcon u3I = new ImageIcon(clder.getResource("u3.jpg"));
-		ImageIcon u4I = new ImageIcon(clder.getResource("u4.jpg"));
-		ImageIcon u5I = new ImageIcon(clder.getResource("u5.jpg"));
-		ImageIcon u6I = new ImageIcon(clder.getResource("u6.jpg"));
-		ImageIcon u7I = new ImageIcon(clder.getResource("u7.jpg"));
-		ImageIcon u8I = new ImageIcon(clder.getResource("u8.jpg"));
-		ImageIcon backgroundIcon = new ImageIcon(clder.getResource("background.jpg"));
-		ImageIcon basepp = new ImageIcon(clder.getResource("base1.jpg"));
-		ImageIcon baseee = new ImageIcon(clder.getResource("base2.jpg"));
-		u11=u1I.getImage();
-		u22=u2I.getImage();
-		u33=u3I.getImage();
-		u44=u4I.getImage();
-		u55=u5I.getImage();
-		u66=u6I.getImage();
-		u77=u7I.getImage();
-		u88=u8I.getImage();
-		background=backgroundIcon.getImage();
-		baseP=basepp.getImage();
-		baseE=baseee.getImage();
-		
+        ClassLoader clder= this.getClass().getClassLoader();
+        ImageIcon u1I = new ImageIcon(clder.getResource("1u.jpg"));
+        ImageIcon u2I = new ImageIcon(clder.getResource("2u.jpg"));
+        ImageIcon u3I = new ImageIcon(clder.getResource("3u.jpg"));
+        ImageIcon u4I = new ImageIcon(clder.getResource("u4.jpg"));
+        ImageIcon u5I = new ImageIcon(clder.getResource("u5.jpg"));
+        ImageIcon u6I = new ImageIcon(clder.getResource("u6.jpg"));
+        ImageIcon u7I = new ImageIcon(clder.getResource("u7.jpg"));
+        ImageIcon u8I = new ImageIcon(clder.getResource("u8.jpg"));
+       //ImageIcon backgroundIcon = new ImageIcon(clder.getResource("background.jpg"));
+        ImageIcon basepp = new ImageIcon(clder.getResource("1base.jpg"));
+        ImageIcon baseee = new ImageIcon(clder.getResource("base2.jpg"));
+        u11=u1I.getImage();
+        u22=u2I.getImage();
+        u33=u3I.getImage();
+        u44=u4I.getImage();
+        u55=u5I.getImage();
+        u66=u6I.getImage();
+        u77=u7I.getImage();
+        u88=u8I.getImage();
+      //  background=backgroundIcon.getImage();
+        baseP=basepp.getImage();
+        baseE=baseee.getImage();
+
 		//
 		SB= new JButton("Start Button");
 		SB.addActionListener(new ButtonHandler());
@@ -121,7 +136,7 @@ public class gui2 extends JFrame {
 		u5.addActionListener(new ButtonHandler());
 		u6 = new JButton(u6I);
 		u6.addActionListener(new ButtonHandler());
-		u7 = new JButton(u7I);
+	    u7 = new JButton(u7I);
 		u7.addActionListener(new ButtonHandler());
 		txtpnResources = new JTextPane();
 		txtpnResources.setText("Resources");
@@ -133,118 +148,120 @@ public class gui2 extends JFrame {
 		label.setBackground(Color.WHITE);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-		gl_contentPane.createParallelGroup(Alignment.TRAILING)
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addContainerGap()
-		.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addComponent(label, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-		.addPreferredGap(ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
-		.addComponent(u1, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addGap(18)
-		.addComponent(u2, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addGap(18)
-		.addComponent(u3, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addGap(18))
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addComponent(txtpnHealth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		.addPreferredGap(ComponentPlacement.RELATED)))
-		.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-		.addComponent(txtpnResources, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u4, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
-		.addGap(18)
-		.addComponent(u5, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addGap(18)
-		.addComponent(u6, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addGap(18)
-		.addComponent(u7, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-		.addPreferredGap(ComponentPlacement.RELATED)
-		.addComponent(SB, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
+							.addComponent(u1, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(u2, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(u3, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+							.addGap(18))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(txtpnHealth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(txtpnResources, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(u4, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(u5, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(u6, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(u7, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(SB, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
 		);
 		gl_contentPane.setVerticalGroup(
-		gl_contentPane.createParallelGroup(Alignment.LEADING)
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addContainerGap()
-		.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addComponent(txtpnHealth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		.addGap(405)
-		.addComponent(label, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE)
-		.addContainerGap())
-		.addGroup(gl_contentPane.createSequentialGroup()
-		.addComponent(txtpnResources, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-		.addPreferredGap(ComponentPlacement.RELATED, 389, Short.MAX_VALUE)
-		.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-		.addComponent(SB, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-		.addComponent(u7, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u6, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u5, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u4, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u3, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u2, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-		.addComponent(u1, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))))))
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(txtpnHealth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(405)
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(txtpnResources, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 389, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(SB, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+									.addComponent(u7, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u6, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u5, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u4, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u3, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u2, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addComponent(u1, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))))))
 		);
-		
-		addWindowListener(new java.awt.event.WindowAdapter() {
-			public void windowClosing(WindowEvent evt) {
-				System.exit(0);
-			}
-		});
-		// STEP 6: set window size and show window
-		contentPane.setSize(1800,1000);
+
+    	addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                System.exit(0);
+            }
+        });
+    	// STEP 6: set window size and show window
+        contentPane.setSize(1800,1000);
 		contentPane.setLayout(gl_contentPane);
 		contentPane.setVisible(true);
 	}
-	
-	public void intructions()
-        {
-            button = new JButton("Instructions");
-	    if(button.isSelected())
-	    {
-	       text = new JTextArea("*Insert instructions*");
-	    }
-        }
-       	//////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
 	public void paint(Graphics g)
 	{
 		super.paint(g);
-		
-		g.drawImage(u11, getX(), getY(), this);
+		if(b1 == true)
+		{
+			g.drawImage(u11, getX(), getY(), this);
+		}
+		if (b2 == true)
+		{
+			g.drawImage(u22,  getX(),  getY(),  this);
+		}
 		
 	}
 	
 	private class ButtonHandler implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
+			
 			
 			if(event.getSource() == u1)
 			{
-				update.createUnit(1);
-				b1 = true;
-				//need a method here to paint the unit onto the screen
+					update.createUnit(1);
+					b1 = true;
+					//need a method here to paint the unit onto the screen
 			}
 			else if(event.getSource() == u2)
-			update.createUnit(2);
+			{
+				update.createUnit(2);
+				b2 = true;
+			}
 			else if(event.getSource() == u3)
-			update.createUnit(3);
+				update.createUnit(3);
 			else if(event.getSource() == u4)
-			update.createUnit(4);
+				update.createUnit(4);
 			else if(event.getSource() == u5)
-			update.createUnit(5);
+				update.createUnit(5);
 			else if(event.getSource() == u6)
-			update.createUnit(6);
+				update.createUnit(6);
 			else if(event.getSource() == u7)
-			update.createUnit(7);
+				update.createUnit(7);
 			/////////////////////////////////////////////////////////////////////////////
 			
 			if(event.getSource() == SB)
 			{
-				//gl_contentPane.removeLayoutComponent(SB);
 				
+						     
+				//gl_contentPane.removeLayoutComponent(SB);
+
 				ActionListener taskPreformer= new ActionListener()
 				{
-					public void actionPerformed(ActionEvent evt)
+					public void actionPerformed(ActionEvent evt) 
 					{
 						// construction a Swing timer that goes off every 1000 msec (1 sec)
 						//update every second
@@ -254,10 +271,27 @@ public class gui2 extends JFrame {
 						eAry=update.getEnemyUnitArray();
 						txtpnResources.setText("Resources:" + r);
 						repaint();
+						//check win
+						int win= update.checkWin();
+						if(win==2)
+						{
+							timer.stop();
+							JOptionPane.showMessageDialog(contentPane, "You Win");
+							System.exit(0);
+						}
+						if(win==1)
+						{
+							timer.stop();
+							JOptionPane.showMessageDialog(contentPane, "You Lose");
+							System.exit(0);
+						}
 					}
-					
 				};
+
+				timer = new javax.swing.Timer(1000, taskPreformer);
+				timer.start();// timer starts here
 			}
 		}
 	}
 }
+	
