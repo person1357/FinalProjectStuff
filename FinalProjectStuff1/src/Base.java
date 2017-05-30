53177ae0257f2acd06ff03bdf5affc80190c7733
Yuanning Wu <cindywu0130@gmail.com>
2017-05-30 15:26:23 -0700

    this is working

--- FinalProjectStuff1/src/Base.java
+++ FinalProjectStuff1/src/Base.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class Base 
 {
 	private int health;
@@ -34,40 +34,4 @@ public class Base
 	}
 	
 }
-=======
-public class Base 
-{
-	private int health;
-	private int location;
-	public Base(int loc)
-	{
-		health = 400;
-		location = loc;
-	}
-	public void setHealth(int value)
-	{
-		health = value;
-	}
-	public void reduceHealth(int val)
-	{
-		health = health - val;
-	}
-	public int getHealth()
-	{
-		return health;
-	}
-	public void setLocatin(int l)
-	{
-		location = l;
-	}
-	public int getLocation() {
-		return location;
-	}
-	//need this method to let GUI display health value in textArea
-	public String toString()
-	{
-		return Integer.toString(health);
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/person1357/FinalProjectStuff
+
